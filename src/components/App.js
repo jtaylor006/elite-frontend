bcd7262cd715ebd59db4359bc747d408d598bd76
jtaylor6 <jarrede20@gmail.com>
2020-05-14 20:43:45 -0400

    set up

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './containers/Header';
+import Body from './containers/Body';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Header />
+      </header>
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/containers/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Body() {
+    return (
+        <p>
+           This is the body portion. We Here. Elite World Wide. 
+        </p>
+    )
+}
+export default Body;
